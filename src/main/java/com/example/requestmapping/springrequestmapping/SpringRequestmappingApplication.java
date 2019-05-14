bf4e114c02cc2ec36fdbc9fc50f6dcaeaf384d10
Mak Patil <makrandapatil17@gmail.com>
2019-05-13 23:29:51 -0700

    request mapping demo in spring boot

---
+++ src/main/java/com/example/requestmapping/springrequestmapping/SpringRequestmappingApplication.java
@@ -0,0 +1,13 @@
+package com.example.requestmapping.springrequestmapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRequestmappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRequestmappingApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/requestmapping/springrequestmapping/UserController.java
@@ -0,0 +1,43 @@
+package com.example.requestmapping.springrequestmapping;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@GetMapping("/")
+	public String index()
+	{
+		return "Hello User From Index!";
+	}
+	
+	@RequestMapping("/hello")
+	public String helloUser()
+	{
+		return "Hello, User!";
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public String displayUserId(@PathVariable("id") int id)
+	{
+		return "User Id : " + id;
+	}
+	
+	@GetMapping("/{name}")
+	public String displayUserName(@PathVariable("name") String name)
+	{
+		return "Hello, " + name;
+	}
+	
+	@GetMapping("/city")
+	public String displayUserCount(@RequestParam("city") String city)
+	{
+		return "User's city is " + city;
+	}
+}
